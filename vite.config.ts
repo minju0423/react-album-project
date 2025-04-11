a95470788c4cd86c47caffabe5c121987603902c
minju0423 <lmj042398@gmail.com>
2025-04-11 22:47:52 +0900

    config 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {fileUrlToPath, URL} from 'url'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve : {
+    alias : {
+      '@' : fileUrlToPath(new URL('./src', import.meta.url)),
+      '@assets' : fileUrlToPath(new URL('./src/assets', import.meta.url)),
+      '@components' : fileUrlToPath(new URL('./src/components', import.meta.url)),
+      '@pages' : fileUrlToPath(new URL('./src/pages', import.meta.url)),
+      '@recoil' : fileUrlToPath(new URL('./src/recoil', import.meta.url)),
+      '@apis' : fileUrlToPath(new URL('./src/apis', import.meta.url)),
+    }
+  },
+  //scss 전역사용
+  css : {
+    preprocessorOptions : {
+      scss : {
+        additionalData : `@import "./src/assets/styles/main.scss`,
+        
+      }
+    }
+  }
 })
