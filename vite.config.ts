aa0d04caa9faa1fa850150703a90d030c79126dd
minju0423 <lmj042398@gmail.com>
2025-04-11 22:48:18 +0900

    vite.config 수정2

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import {fileUrlToPath, URL} from 'url'
+import {fileURLToPath, URL} from 'url'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve : {
     alias : {
-      '@' : fileUrlToPath(new URL('./src', import.meta.url)),
-      '@assets' : fileUrlToPath(new URL('./src/assets', import.meta.url)),
-      '@components' : fileUrlToPath(new URL('./src/components', import.meta.url)),
-      '@pages' : fileUrlToPath(new URL('./src/pages', import.meta.url)),
-      '@recoil' : fileUrlToPath(new URL('./src/recoil', import.meta.url)),
-      '@apis' : fileUrlToPath(new URL('./src/apis', import.meta.url)),
+      '@' : fileURLToPath(new URL('./src', import.meta.url)),
+      '@assets' : fileURLToPath(new URL('./src/assets', import.meta.url)),
+      '@components' : fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@pages' : fileURLToPath(new URL('./src/pages', import.meta.url)),
+      '@recoil' : fileURLToPath(new URL('./src/recoil', import.meta.url)),
+      '@apis' : fileURLToPath(new URL('./src/apis', import.meta.url)),
     }
   },
   //scss 전역사용
@@ -20,7 +20,7 @@ export default defineConfig({
     preprocessorOptions : {
       scss : {
         additionalData : `@import "./src/assets/styles/main.scss`,
-        
+
       }
     }
   }
