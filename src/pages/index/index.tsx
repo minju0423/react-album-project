6cbb9a8701201032f5426093378b1587be5eb38d
minju0423 <lmj042398@gmail.com>
2025-05-26 12:27:32 +0900

    refactor : 이미지 호출 useMemo로 변경

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -8,20 +8,31 @@ import Card from "./components/Card"
 import styles from "./styles/index.module.scss"
 // import { useState } from "react"
 import { CardDTO } from "./types/card"
-import { useRecoilValue } from "recoil"
+import { useRecoilValueLoadable } from "recoil"
 import { imageData } from "@/recoil/seletors/imageSelector"
-import { useState } from "react"
+import { useMemo, useState } from "react"
 
 function index() {
-    const imgSelector = useRecoilValue(imageData) // recoil selector 호출
+    
+    const imgSelector = useRecoilValueLoadable(imageData)
     const [imgData, setImgData] = useState<CardDTO>()
 
     const [open, setOpen] = useState<boolean>(false) //이미지 상세 다이얼로그 발생(관리) State
-    const CARD_LIST = imgSelector.data.results.map((card : CardDTO)=>{
-        return (
-            <Card data={card} key={card.id} handleDialog={setOpen} handleSetData={setImgData}/>
-        )
-    })
+    
+
+    const CARD_LIST = useMemo(() => {
+        //imageSeletor.state = hasValue or loading
+        if(imgSelector.state === 'hasValue'){
+            const result = imgSelector.contents.map((card : CardDTO)=>{
+                return (
+                    <Card data={card} key={card.id} handleDialog={setOpen} handleSetData={setImgData}/>
+                )
+            })
+            return result
+        }else{
+            return <div>loading...</div>
+        }
+    }, [imgSelector])
     
     return (
         <div className={styles.page}> 
