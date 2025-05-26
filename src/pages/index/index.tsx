c3a303feb200b9f234f8674985f3548c34a39b6d
minju0423 <125239229+minju0423@users.noreply.github.com>
2025-05-26 13:08:39 +0900

    [Day/0526] Detail Dialog, Navigation, SearchBar 기능 구현 (#6)
    * feat : 디테일 다이얼로그 생성
    
    * feat : dialog 열기 이벤트 생성
    
    * feat : 상세 다이얼로그 데이터 바인딩
    
    * refactor : 이미지 호출 useMemo로 변경
    
    * feat : navigation에 따른 데이터 바인딩
    
    * feat: search bar 이벤트 생성

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -2,23 +2,37 @@ import CommonFooter from "@/components/common/footer/CommonFooter"
 import CommonHeader from "@/components/common/header/CommonHeader"
 import CommonNav from "@/components/common/navigation/CommonNav"
 import CommonSearchBar from "@/components/common/searchBar/CommonSearchBar"
+import DetailDialog from "@/components/common/dialog/DetailDialog"
 import Card from "./components/Card"
 
 import styles from "./styles/index.module.scss"
 // import { useState } from "react"
 import { CardDTO } from "./types/card"
-import { useRecoilValue } from "recoil"
+import { useRecoilValueLoadable } from "recoil"
 import { imageData } from "@/recoil/seletors/imageSelector"
+import { useMemo, useState } from "react"
 
 function index() {
-    const imgSelector = useRecoilValue(imageData) // recoil selector 호출
-    // const [imgData, setImgData] = useState<CardDTO[]>([])
+    
+    const imgSelector = useRecoilValueLoadable(imageData)
+    const [imgData, setImgData] = useState<CardDTO>()
 
-    const CARD_LIST = imgSelector.data.results.map((card : CardDTO)=>{
-        return (
-            <Card data={card} key={card.id}/>
-        )
-    })
+    const [open, setOpen] = useState<boolean>(false) //이미지 상세 다이얼로그 발생(관리) State
+    
+
+    const CARD_LIST = useMemo(() => {
+        //imageSeletor.state = hasValue or loading
+        if(imgSelector.state === 'hasValue'){
+            const result = imgSelector.contents.map((card : CardDTO)=>{
+                return (
+                    <Card data={card} key={card.id} handleDialog={setOpen} handleSetData={setImgData}/>
+                )
+            })
+            return result
+        }else{
+            return <div>loading...</div>
+        }
+    }, [imgSelector])
     
     return (
         <div className={styles.page}> 
@@ -44,6 +58,7 @@ function index() {
             </div>
             {/**공통 푸터 UI 부분 */}
             <CommonFooter/>
+            {open && <DetailDialog data={imgData} handleDialog={setOpen}/>}
         </div>
     )
 }
